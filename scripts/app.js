26f1199ea59efa9b15766854f0b05272c9c91d11
Joel Brewster <hi@joelbrewster.com>
2021-02-27 16:15:07 +1100

    try defer script

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const body = document.querySelector('body');
-    body.className = 'theme-1';
+const body = document.querySelector('body');
+body.className = 'theme-1';
 
-    const header =  document.querySelector('header');
-    header.insertAdjacentHTML('afterend', '<button class="theme-toggle">cycle themes</button>');
+const header =  document.querySelector('header');
+header.insertAdjacentHTML('afterend', '<button class="theme-toggle">CYCLE THEMES</button>');
 
-    const trigger = document.querySelector(".theme-toggle");
-    trigger.addEventListener("click", () => {
-	changeBackground();
-    });
-
-    function changeBackground() {
-	if(body.classList.contains("theme-1")){
-	    body.classList.remove("theme-1");
-	    body.classList.add("theme-2");
-	} else if(body.classList.contains("theme-2")){
-	    body.classList.remove("theme-2");
-	    body.classList.add("theme-3");
-	} else if(body.classList.contains("theme-3")){
-	    body.classList.remove("theme-3");
-	    body.classList.add("theme-1");
-	}
-    }
+const trigger = document.querySelector(".theme-toggle");
+trigger.addEventListener("click", () => {
+    changeBackground();
 });
+
+function changeBackground() {
+    if(body.classList.contains("theme-1")){
+	body.classList.remove("theme-1");
+	body.classList.add("theme-2");
+    } else if(body.classList.contains("theme-2")){
+	body.classList.remove("theme-2");
+	body.classList.add("theme-3");
+    } else if(body.classList.contains("theme-3")){
+	body.classList.remove("theme-3");
+	body.classList.add("theme-1");
+    }
+}
